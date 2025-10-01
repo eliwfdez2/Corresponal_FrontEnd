e48d76da9451d39e39d6be478d1e1cf14f6404b4
eliwfdez2 <eliw@rodall.com>
2025-10-01 17:37:51 -0600

    se trae el listado de los extension archivos y conceptos

--- src/app/Modals/subir-archivos/subir-archivos.component.ts
+++ src/app/Modals/subir-archivos/subir-archivos.component.ts
@@ -1,19 +1,38 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ConceptosService } from '../../core/services/conceptos.service';
+import { ExtensionesArchivosService } from '../../core/services/extensiones-archivos.service';
 
 @Component({
   selector: 'app-subir-archivos',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './subir-archivos.component.html',
   styleUrl: './subir-archivos.component.css'
 })
-export class SubirArchivosComponent {
+export class SubirArchivosComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
-  @Output() upload = new EventEmitter<File[]>();
+  @Output() upload = new EventEmitter<{file: File, selectedConcept: string, selectedExtension: string}[]>();
 
-  uploadedFiles: File[] = [];
+  uploadedFiles: {file: File, selectedConcept: string, selectedExtension: string}[] = [];
   showConfirmation: boolean = false;
   confirmationStep: number = 0; // 0: none, 1: first confirm, 2: second confirm
+  conceptos: string[] = [];
+  extensiones: string[] = [];
+
+  constructor(
+    private conceptosService: ConceptosService,
+    private extensionesService: ExtensionesArchivosService
+  ) {}
+
+  ngOnInit() {
+    this.conceptosService.getConceptos().subscribe(data => {
+      this.conceptos = data.filter(c => c.activo == 1).map(c => c.clave);
+    });
+    this.extensionesService.getExtensionesArchivos().subscribe(data => {
+      this.extensiones = data.filter(e => e.activo == 1).map(e => e.extension_archivo);
+    });
+  }
 
   closeModal() {
     this.close.emit();
@@ -33,7 +52,8 @@ export class SubirArchivosComponent {
     event.preventDefault();
     event.stopPropagation();
     if (event.dataTransfer?.files) {
-      this.uploadedFiles.push(...Array.from(event.dataTransfer.files));
+      const files = Array.from(event.dataTransfer.files).map(file => ({ file, selectedConcept: '', selectedExtension: '' }));
+      this.uploadedFiles.push(...files);
     }
   }
 
@@ -44,7 +64,8 @@ export class SubirArchivosComponent {
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files) {
-      this.uploadedFiles.push(...Array.from(input.files));
+      const files = Array.from(input.files).map(file => ({ file, selectedConcept: '', selectedExtension: '' }));
+      this.uploadedFiles.push(...files);
       input.value = '';
     }
   }
